27450878f53453b90fcfdffd4e66d6c97a1e2fd7
Yasin Bağçuvan <yasinbagcuvan@gmail.com>
2025-08-09 16:03:29 +0300

    index içinden redux kaldırıldı.

--- apps/basket-remote1/src/index.tsx
+++ apps/basket-remote1/src/index.tsx
@@ -1,8 +1,5 @@
-// basket-remote/src/index.tsx
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(<App />);
